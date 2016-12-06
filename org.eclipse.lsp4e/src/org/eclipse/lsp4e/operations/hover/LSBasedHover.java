0b9f8c89e4a4f4b4ea5dbe754650890307df9e0a
Michal Niewrzal <michal.n@zend.com>
2016-12-06 13:50:32 +0100

    Bug 508486 - Hovers must be rendered as markdown
    Change-Id: Iacaf70034b0c6e53224d0fc51fabcba5dc482107
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/hover/LSBasedHover.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/hover/LSBasedHover.java
@@ -14,9 +14,16 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
+import org.eclipse.jface.internal.text.html.BrowserInformationControl;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.text.DefaultInformationControl;
+import org.eclipse.jface.text.IInformationControl;
+import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextHover;
+import org.eclipse.jface.text.ITextHoverExtension;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.Region;
 import org.eclipse.lsp4e.LSPEclipseUtils;
@@ -24,9 +31,14 @@ import org.eclipse.lsp4e.LanguageServiceAccessor;
 import org.eclipse.lsp4e.LanguageServiceAccessor.LSPDocumentInfo;
 import org.eclipse.lsp4j.Hover;
 import org.eclipse.lsp4j.Range;
+import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
+import org.eclipse.mylyn.wikitext.markdown.core.MarkdownLanguage;
+import org.eclipse.swt.widgets.Shell;
 
-public class LSBasedHover implements ITextHover {
+public class LSBasedHover implements ITextHover, ITextHoverExtension {
 
+	private static final MarkupParser MARKDOWN_PARSER = new MarkupParser(new MarkdownLanguage());
+	
 	private CompletableFuture<Hover> hoverRequest;
 	private IRegion lastRegion;
 	private ITextViewer textViewer;
@@ -39,7 +51,7 @@ public class LSBasedHover implements ITextHover {
 		if (!(hoverRegion.equals(this.lastRegion) && textViewer.equals(this.textViewer) && this.hoverRequest != null)) {
 			initiateHoverRequest(textViewer, hoverRegion.getOffset());
 		}
-		if (this.hoverRequest == null){
+		if (this.hoverRequest == null) {
 			return null;
 		}
 		Hover hoverResult = null;
@@ -51,13 +63,8 @@ public class LSBasedHover implements ITextHover {
 		if (hoverResult == null) {
 			return null;
 		}
-		StringBuilder res = new StringBuilder();
-		for (String string : hoverResult.getContents()) {
-			res.append(string);
-			res.append('\n');
-		}
-
-		return res.toString();
+		String result = hoverResult.getContents().stream().collect(Collectors.joining("\n\n")); //$NON-NLS-1$
+		return MARKDOWN_PARSER.parseToHtml(result);
 	}
 
 	@Override
@@ -91,9 +98,25 @@ public class LSBasedHover implements ITextHover {
 		if (info != null) {
 			try {
 				this.hoverRequest = info.getLanguageClient().getTextDocumentService().hover(LSPEclipseUtils.toTextDocumentPosistionParams(info.getFileUri(), offset, info.getDocument()));
-			}  catch (Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	@Override
+	public IInformationControlCreator getHoverControlCreator() {
+		return new IInformationControlCreator() {
+
+			@Override
+			public IInformationControl createInformationControl(Shell parent) {
+				// DefaultInformationControl is not able to render HTML use browser version if available
+				if (BrowserInformationControl.isAvailable(parent)) {
+					return new BrowserInformationControl(parent, JFaceResources.DEFAULT_FONT, true);
+				} else {
+					return new DefaultInformationControl(parent);
+				}
+			}
+		};
 	}
 }
