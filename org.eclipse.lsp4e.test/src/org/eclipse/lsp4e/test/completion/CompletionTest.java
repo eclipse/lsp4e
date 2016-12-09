821faa5e0f3bbd93ff166f367aec17dacc189ca3
Michal Niewrzal <michal.n@zend.com>
2016-12-09 13:11:35 +0100

    Bug 508602- Completion test improvements
    * added tests for applying proposals
    * small code reorg to simplify test cases
    
    Change-Id: Idd69ae9eebfd4c7c8171dcba9047951b66c85952
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- org.eclipse.lsp4e.test/src/org/eclipse/lsp4e/test/completion/CompletionTest.java
+++ org.eclipse.lsp4e.test/src/org/eclipse/lsp4e/test/completion/CompletionTest.java
@@ -40,13 +40,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+@SuppressWarnings("restriction")
 public class CompletionTest {
 
 	private IProject project;
+	private LSContentAssistProcessor contentAssistProcessor;
 
 	@Before
 	public void setUp() throws CoreException {
 		project = TestUtils.createProject("CompletionTest" + System.currentTimeMillis());
+		contentAssistProcessor = new LSContentAssistProcessor();
 	}
 
 	@After
@@ -60,15 +63,13 @@ public class CompletionTest {
 			IllegalArgumentException, InvocationTargetException {
 		List<CompletionItem> items = new ArrayList<>();
 		items.add(createCompletionItem("FirstClass", CompletionItemKind.Class));
-		CompletionList completionList = new CompletionList(false, items);
-		MockLanguageSever.INSTANCE.setCompletionList(completionList);
+		MockLanguageSever.INSTANCE.setCompletionList(new CompletionList(false, items));
 
 		IFile testFile = project.getFile("test01.lspt");
 		testFile.create(new ByteArrayInputStream(new byte[0]), true, null);
 
 		ITextViewer viewer = TestUtils.openTextViewer(testFile);
 
-		LSContentAssistProcessor contentAssistProcessor = new LSContentAssistProcessor();
 		ICompletionProposal[] proposals = contentAssistProcessor.computeCompletionProposals(viewer, 0);
 		assertEquals(items.size(), proposals.length);
 		// TODO compare both structures
@@ -82,65 +83,107 @@ public class CompletionTest {
 		items.add(createCompletionItem("SecondClass", CompletionItemKind.Class));
 		MockLanguageSever.INSTANCE.setCompletionList(new CompletionList(false, items));
 
-		IFile testFile = project.getFile("test02.lspt");
 		String content = "First";
-		testFile.create(new ByteArrayInputStream(content.getBytes()), true, null);
+		ITextViewer viewer = TestUtils.openTextViewer(createUniqueTestFile(content));
 
-		ITextViewer viewer = TestUtils.openTextViewer(testFile);
-
-		LSContentAssistProcessor contentAssistProcessor = new LSContentAssistProcessor();
 		ICompletionProposal[] proposals = contentAssistProcessor.computeCompletionProposals(viewer, content.length());
 		assertEquals(1, proposals.length);
 		// TODO compare items
 	}
-	
+
 	@Test
 	public void testCompleteOnFileEnd() throws Exception { // bug 508842
-		List<CompletionItem> items = new ArrayList<>();
 		CompletionItem item = new CompletionItem();
 		item.setLabel("1024M");
 		item.setKind(CompletionItemKind.Value);
 		item.setTextEdit(new TextEdit(new Range(new Position(2, 10), new Position(2, 10)), "1024M"));
 		CompletionList completionList = new CompletionList(false, Collections.singletonList(item));
-		MockLanguageSever.INSTANCE.setCompletionList(completionList);	
+		MockLanguageSever.INSTANCE.setCompletionList(completionList);
 
-		IFile testFile = project.getFile("test03.lspt");
-		String content = "applications:\n" + 
-				"- name: hello\n" + 
-				"  memory: ";
-		testFile.create(new ByteArrayInputStream(content.getBytes()), true, null);
+		String content = "applications:\n" + "- name: hello\n" + "  memory: ";
+		ITextViewer viewer = TestUtils.openTextViewer(createUniqueTestFile(content));
 
-		ITextViewer viewer = TestUtils.openTextViewer(testFile);
-
-		LSContentAssistProcessor contentAssistProcessor = new LSContentAssistProcessor();
 		ICompletionProposal[] proposals = contentAssistProcessor.computeCompletionProposals(viewer, content.length());
 		assertEquals(1, proposals.length);
 		proposals[0].apply(viewer.getDocument());
 		assertEquals(content + "1024M", viewer.getDocument().get());
 	}
 
-	private CompletionItem createCompletionItem(String label, CompletionItemKind kind) {
-		CompletionItem item = new CompletionItem();
-		item.setLabel(label);
-		item.setKind(kind);
-		item.setTextEdit(new TextEdit(new Range(new Position(0, 0), new Position(0, label.length())), label));
-		return item;
-	}
-	
 	@Test
-	public void testTriggerCharsWithoutPreliminaryCompletion() throws Exception  { // bug 508463
+	public void testTriggerCharsWithoutPreliminaryCompletion() throws Exception { // bug 508463
 		Set<String> triggers = new HashSet<>();
 		triggers.add("a");
 		triggers.add("b");
 		MockLanguageSever.INSTANCE.setCompletionTriggerChars(triggers);
-		IFile testFile = project.getFile("test02.lspt");
+
 		String content = "First";
+		TestUtils.openTextViewer(createUniqueTestFile(content));
+
+		assertArrayEquals(new char[] { 'a', 'b' },
+				contentAssistProcessor.getCompletionProposalAutoActivationCharacters());
+	}
+
+	@Test
+	public void testApplyCompletion() throws Exception {
+		Range range = new Range(new Position(0, 0), new Position(0, 0));
+		List<CompletionItem> items = Collections
+				.singletonList(createCompletionItem("FirstClass", CompletionItemKind.Class, range));
+		MockLanguageSever.INSTANCE.setCompletionList(new CompletionList(false, items));
+
+		String content = "";
+		ITextViewer viewer = TestUtils.openTextViewer(createUniqueTestFile(content));
+
+		ICompletionProposal[] proposals = contentAssistProcessor.computeCompletionProposals(viewer, content.length());
+		proposals[0].apply(viewer.getDocument());
+		assertEquals(true, viewer.getDocument().get().equals("FirstClass"));
+	}
+
+	@Test
+	public void testApplyCompletionWithPrefix() throws Exception {
+		Range range = new Range(new Position(0, 0), new Position(0, 5));
+		List<CompletionItem> items = Collections
+				.singletonList(createCompletionItem("FirstClass", CompletionItemKind.Class, range));
+		MockLanguageSever.INSTANCE.setCompletionList(new CompletionList(false, items));
+
+		String content = "First";
+		ITextViewer viewer = TestUtils.openTextViewer(createUniqueTestFile(content));
+
+		ICompletionProposal[] proposals = contentAssistProcessor.computeCompletionProposals(viewer, content.length());
+		proposals[0].apply(viewer.getDocument());
+		assertEquals(true, viewer.getDocument().get().equals("FirstClass"));
+	}
+
+	@Test
+	public void testApplyCompletionReplace() throws Exception {
+		Range range = new Range(new Position(0, 0), new Position(0, 20));
+		List<CompletionItem> items = Collections
+				.singletonList(createCompletionItem("FirstClass", CompletionItemKind.Class, range));
+		MockLanguageSever.INSTANCE.setCompletionList(new CompletionList(false, items));
+
+		String content = "FirstNotMatchedLabel";
+		ITextViewer viewer = TestUtils.openTextViewer(createUniqueTestFile(content));
+
+		ICompletionProposal[] proposals = contentAssistProcessor.computeCompletionProposals(viewer, 5);
+		proposals[0].apply(viewer.getDocument());
+		assertEquals(true, viewer.getDocument().get().equals("FirstClass"));
+	}
+
+	private IFile createUniqueTestFile(String content) throws CoreException {
+		IFile testFile = project.getFile("test" + (System.currentTimeMillis()) + ".lspt");
 		testFile.create(new ByteArrayInputStream(content.getBytes()), true, null);
+		return testFile;
+	}
 
-		TestUtils.openTextViewer(testFile);
+	private CompletionItem createCompletionItem(String label, CompletionItemKind kind) {
+		return createCompletionItem(label, kind, new Range(new Position(0, 0), new Position(0, label.length())));
+	}
 
-		LSContentAssistProcessor contentAssistProcessor = new LSContentAssistProcessor();
-		assertArrayEquals(new char[] {'a', 'b'}, contentAssistProcessor.getCompletionProposalAutoActivationCharacters());
+	private CompletionItem createCompletionItem(String label, CompletionItemKind kind, Range range) {
+		CompletionItem item = new CompletionItem();
+		item.setLabel(label);
+		item.setKind(kind);
+		item.setTextEdit(new TextEdit(range, label));
+		return item;
 	}
 
 }
