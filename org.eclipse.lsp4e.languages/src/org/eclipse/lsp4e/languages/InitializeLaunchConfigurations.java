35a5984efa2f18c204ecd4702303bcb948908920
Mickael Istria <mistria@redhat.com>
2017-02-09 12:04:44 +0100

    Removed OmniSharp as it's covered by aCute project
    Change-Id: I98309013aca0f0d7b5eb3c63763ceb341b3f20b5
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- org.eclipse.lsp4e.languages/src/org/eclipse/lsp4e/languages/InitializeLaunchConfigurations.java
+++ org.eclipse.lsp4e.languages/src/org/eclipse/lsp4e/languages/InitializeLaunchConfigurations.java
@@ -17,8 +17,6 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.externaltools.internal.IExternalToolConstants;
@@ -46,7 +44,6 @@ public class InitializeLaunchConfigurations implements IStartup {
 
 	public static final String VSCODE_CSS_NAME = "VSCode-CSS";
 	public static final String VSCODE_JSON_NAME = "VSCode-JSON";
-	public static final String OMNISHARP_NAME = "OmniSharp";
 
 	@Override
 	public void earlyStartup() {
@@ -54,81 +51,6 @@ public class InitializeLaunchConfigurations implements IStartup {
 		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
 		ILaunchConfigurationType externalType = launchManager.getLaunchConfigurationType(IExternalToolConstants.ID_PROGRAM_LAUNCH_CONFIGURATION_TYPE);
 		LSPStreamConnectionProviderRegistry registry = LSPStreamConnectionProviderRegistry.getInstance();
-		// OmniSharp
-		try {
-			String omniSharpLaunchName = OMNISHARP_NAME;
-			ILaunchConfiguration omniSharpLauch = null;
-			for (ILaunchConfiguration launch : launchManager.getLaunchConfigurations(externalType)) {
-				if (launch.getName().equals(omniSharpLaunchName)) {
-					omniSharpLauch = launch;
-				}
-			}
-			if (omniSharpLauch == null) {
-				ILaunchConfigurationWorkingCopy workingCopy = externalType.newInstance(null, omniSharpLaunchName);
-				// some common config
-				workingCopy.setAttribute(IExternalToolConstants.ATTR_LAUNCH_IN_BACKGROUND, true);
-				workingCopy.setAttribute(IExternalToolConstants.ATTR_BUILDER_ENABLED, false);
-				workingCopy.setAttribute(IExternalToolConstants.ATTR_SHOW_CONSOLE, false);
-				workingCopy.setAttribute(IExternalToolConstants.ATTR_BUILD_SCOPE, "${none}");
-				workingCopy.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
-				workingCopy.setAttribute(IExternalToolConstants.ATTR_LOCATION, getNodeJsLocation());
-				workingCopy.setAttribute(IExternalToolConstants.ATTR_TOOL_ARGUMENTS, "/home/mistria/git/omnisharp-node-client/languageserver/server.js");
-				workingCopy.setAttribute(ILaunchManager.ATTR_APPEND_ENVIRONMENT_VARIABLES, true);
-				Map<String, String> environment = new HashMap<>(1);
-				environment.put("LD_LIBRARY_PATH", "/home/mistria/apps/OmniSharp.NET/icu54:" + System.getenv("LD_LIBRARY_PATH"));
-				workingCopy.setAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES, environment);
-				omniSharpLauch = workingCopy.doSave();
-				registry.registerAssociation(contentTypeManager.getContentType("org.eclipse.lsp4e.languages.csharp"),
-						LaunchConfigurationStreamProvider.findLaunchConfiguration(IExternalToolConstants.ID_PROGRAM_LAUNCH_CONFIGURATION_TYPE, InitializeLaunchConfigurations.OMNISHARP_NAME),
-						Collections.singleton(ILaunchManager.RUN_MODE));
-			}
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
-		// VSCode CSS
-//		try {
-//			String omniSharpLaunchName = VSCODE_CSS_NAME;
-//			ILaunchConfiguration omniSharpLauch = null;
-//			for (ILaunchConfiguration launch : launchManager.getLaunchConfigurations(externalType)) {
-//				if (launch.getName().equals(omniSharpLaunchName)) {
-//					omniSharpLauch = launch;
-//				}
-//			}
-//			if (omniSharpLauch == null) {
-//				ILaunchConfigurationWorkingCopy workingCopy = externalType.newInstance(null, omniSharpLaunchName);
-//				// some common config
-//				workingCopy.setAttribute(IExternalToolConstants.ATTR_LAUNCH_IN_BACKGROUND, true);
-//				workingCopy.setAttribute(IExternalToolConstants.ATTR_BUILDER_ENABLED, false);
-//				workingCopy.setAttribute(IExternalToolConstants.ATTR_SHOW_CONSOLE, false);
-//				workingCopy.setAttribute(IExternalToolConstants.ATTR_BUILD_SCOPE, "${none}");
-//				workingCopy.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, false);
-//				workingCopy.setAttribute(IExternalToolConstants.ATTR_LOCATION, getNodeJsLocation());
-//				// Assume node is already installed on machine and uses it
-//				// TODO: implement smarter and multi-platform discovery
-//				workingCopy.setAttribute(IExternalToolConstants.ATTR_TOOL_ARGUMENTS, getVSCodeLocation("/resources/app/extensions/css/server/out/cssServerMain.js") + " --stdio");
-//				omniSharpLauch = workingCopy.doSave();
-//				registry.registerAssociation(contentTypeManager.getContentType("org.eclipse.lsp4e.languages.css"),
-//						LaunchConfigurationStreamProvider.findLaunchConfiguration(IExternalToolConstants.ID_PROGRAM_LAUNCH_CONFIGURATION_TYPE, InitializeLaunchConfigurations.VSCODE_CSS_NAME),
-//						Collections.singleton(ILaunchManager.RUN_MODE));
-//				registry.registerAssociation(contentTypeManager.getContentType("org.eclipse.lsp4e.languages.less"),
-//						LaunchConfigurationStreamProvider.findLaunchConfiguration(IExternalToolConstants.ID_PROGRAM_LAUNCH_CONFIGURATION_TYPE, InitializeLaunchConfigurations.VSCODE_CSS_NAME),
-//						Collections.singleton(ILaunchManager.RUN_MODE));
-//				registry.registerAssociation(contentTypeManager.getContentType("org.eclipse.lsp4e.languages.scss"),
-//						LaunchConfigurationStreamProvider.findLaunchConfiguration(IExternalToolConstants.ID_PROGRAM_LAUNCH_CONFIGURATION_TYPE, InitializeLaunchConfigurations.VSCODE_CSS_NAME),
-//						Collections.singleton(ILaunchManager.RUN_MODE));
-//			}
-//		} catch (CoreException e) {
-//			e.printStackTrace();
-//		}
-//		List<String> commands = new ArrayList<>();
-//		
-//		commands.add(getNodeJsLocation());
-//		commands.add(getVSCodeLocation("/resources/app/extensions/css/server/out/cssServerMain.js"));
-//		commands.add("--stdio");
-//		String workingDir = getVSCodeLocation("/resources/app/extensions/css/server/out");
-//		StreamConnectionProvider provider = new ProcessStreamConnectionProvider(commands, workingDir);
-//		registry.registerAssociation(contentTypeManager.getContentType("org.eclipse.lsp4e.languages.css"), provider);
-		
 		// VSCode CSS
 		try {
 			String omniSharpLaunchName = VSCODE_JSON_NAME;
