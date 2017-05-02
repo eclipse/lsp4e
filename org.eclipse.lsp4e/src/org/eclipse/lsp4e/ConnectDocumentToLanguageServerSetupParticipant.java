926fc9020a7bf05ad3cc1a3b7d10ce2d0696d309
Mickael Istria <mistria@redhat.com>
2017-05-02 16:49:15 +0200

    Better connection with Document
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/ConnectDocumentToLanguageServerSetupParticipant.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/ConnectDocumentToLanguageServerSetupParticipant.java
@@ -14,15 +14,17 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
+import org.eclipse.core.filebuffers.IDocumentSetupParticipantExtension;
 import org.eclipse.core.filebuffers.ITextFileBuffer;
 import org.eclipse.core.filebuffers.ITextFileBufferManager;
+import org.eclipse.core.filebuffers.LocationKind;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.lsp4e.server.StreamConnectionProvider;
 
@@ -30,7 +32,29 @@ import org.eclipse.lsp4e.server.StreamConnectionProvider;
  * Startup the language servers that can be used by the document.
  *
  */
-public class ConnectDocumentToLanguageServerSetupParticipant implements IDocumentSetupParticipant {
+public class ConnectDocumentToLanguageServerSetupParticipant implements IDocumentSetupParticipant, IDocumentSetupParticipantExtension {
+
+	private class DocumentInputStream extends InputStream {
+		private int index = 0;
+		private IDocument document;
+
+		DocumentInputStream(IDocument document) {
+			this.document = document;
+		}
+
+		@Override
+		public int read() throws IOException {
+			if (index < document.getLength()) {
+				try {
+					return document.getChar(index);
+				} catch (BadLocationException e) {
+					throw new IOException(e);
+				}
+			}
+			return -1;
+		}
+
+	}
 
 	@Override
 	public void setup(IDocument document) {
@@ -38,33 +62,38 @@ public class ConnectDocumentToLanguageServerSetupParticipant implements IDocumen
 		if (buffer == null || buffer.getLocation() == null) {
 			return;
 		}
-		IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(buffer.getLocation());
-		if (!resource.exists() || resource.getType() != IResource.FILE) {
-			// only support workspace file so far
-			return;
-		}
-		IFile file = (IFile) resource;
-		IProject project = file.getProject();
+		setup(document, buffer.getLocation(), LocationKind.IFILE);
+	}
+
+	@Override
+	public void setup(IDocument document, IPath location, LocationKind locationKind) {
+		String fileName = location.segment(location.segmentCount() - 1);
 		IContentType[] fileContentTypes = new IContentType[0];
-		try (InputStream contents = file.getContents()) {
-			fileContentTypes = Platform.getContentTypeManager().findContentTypesFor(contents, file.getName()); //TODO consider using document as inputstream
-		} catch (CoreException e) {
-			LanguageServerPlugin.logError(e);
+		try (InputStream contents = new DocumentInputStream(document)) {
+			fileContentTypes = Platform.getContentTypeManager().findContentTypesFor(contents, fileName);
 		} catch (IOException e) {
 			LanguageServerPlugin.logError(e);
 		}
 
-		// create servers one for available content type
-		for (IContentType contentType : fileContentTypes) {
-			for (StreamConnectionProvider connection : LSPStreamConnectionProviderRegistry.getInstance().findProviderFor(contentType)) {
-				if (connection != null) {
-					try {
-						LanguageServiceAccessor.getLSWrapperForConnection(project, contentType, connection);
-					} catch (IOException e) {
-						e.printStackTrace();
+		if (locationKind == LocationKind.IFILE) {
+			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(location);
+			IProject project = file.getProject();
+
+			// create servers one for available content type
+			for (IContentType contentType : fileContentTypes) {
+				for (StreamConnectionProvider connection : LSPStreamConnectionProviderRegistry.getInstance().findProviderFor(contentType)) {
+					if (connection != null) {
+						try {
+							LanguageServiceAccessor.getLSWrapperForConnection(project, contentType, connection);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 			}
+		} else {
+			// only support workspace file so far
+			return;
 		}
 	}
 
