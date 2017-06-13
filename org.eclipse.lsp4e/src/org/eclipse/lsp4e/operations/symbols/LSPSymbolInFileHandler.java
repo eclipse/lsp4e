85c7e5e4bccc6a7714c58a0bc7c7686fb765adf7
Michal Niewrzal <michal.n@zend.com>
2017-06-13 10:00:28 +0200

    Bug 518132 - Change symbols binding to classic Eclipse
    Changed bindings for quick outline and go to symbol in workspace
    
    Change-Id: I757e890c4e8e704091fd32d9d287220c80a5ffb3
    Signed-off-by: Michal Niewrzal <michal.n@zend.com>

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/symbols/LSPSymbolInFileHandler.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/symbols/LSPSymbolInFileHandler.java
@@ -25,6 +25,8 @@ import org.eclipse.lsp4j.SymbolInformation;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.texteditor.ITextEditor;
 
@@ -44,9 +46,10 @@ public class LSPSymbolInFileHandler extends AbstractHandler {
 			// TODO maybe consider better strategy such as iterating on all LS until we have a good result
 			LSPDocumentInfo info = infos.iterator().next();
 			final Shell shell = HandlerUtil.getActiveShell(event);
-			DocumentSymbolParams params = new DocumentSymbolParams(new TextDocumentIdentifier(info.getFileUri().toString()));
+			DocumentSymbolParams params = new DocumentSymbolParams(
+					new TextDocumentIdentifier(info.getFileUri().toString()));
 			CompletableFuture<List<? extends SymbolInformation>> symbols = info.getLanguageClient()
-			        .getTextDocumentService().documentSymbol(params);
+					.getTextDocumentService().documentSymbol(params);
 
 			symbols.thenAccept((List<? extends SymbolInformation> t) -> {
 				shell.getDisplay().asyncExec(() -> {
@@ -58,4 +61,15 @@ public class LSPSymbolInFileHandler extends AbstractHandler {
 		return null;
 	}
 
+	@Override
+	public boolean isEnabled() {
+		IWorkbenchPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
+		if (part instanceof ITextEditor) {
+			List<LSPDocumentInfo> infos = LanguageServiceAccessor.getLSPDocumentInfosFor(
+					LSPEclipseUtils.getDocument((ITextEditor) part),
+					(capabilities) -> Boolean.TRUE.equals(capabilities.getDocumentSymbolProvider()));
+			return !infos.isEmpty();
+		}
+		return false;
+	}
 }
--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/symbols/LSPSymbolInWorkspaceHandler.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/symbols/LSPSymbolInWorkspaceHandler.java
@@ -23,14 +23,17 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.lsp4e.LSPEclipseUtils;
 import org.eclipse.lsp4e.LanguageServiceAccessor;
+import org.eclipse.lsp4e.LanguageServiceAccessor.LSPDocumentInfo;
 import org.eclipse.lsp4j.Location;
 import org.eclipse.lsp4j.SymbolInformation;
 import org.eclipse.lsp4j.services.LanguageServer;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 public class LSPSymbolInWorkspaceHandler extends AbstractHandler {
 
@@ -49,17 +52,17 @@ public class LSPSymbolInWorkspaceHandler extends AbstractHandler {
 			resource = adaptable.getAdapter(IResource.class);
 		}
 
-		if (resource == null) {
-			return null;
-		}
 		IProject project = resource.getProject();
 		List<@NonNull LanguageServer> languageServers = LanguageServiceAccessor.getLanguageServers(project,
 				capabilities -> Boolean.TRUE.equals(capabilities.getWorkspaceSymbolProvider()));
 		if (languageServers.isEmpty()) {
 			return null;
 		}
-		final Shell shell = HandlerUtil.getActiveShell(event);
-		LSPSymbolInWorkspaceDialog dialog = new LSPSymbolInWorkspaceDialog(shell, languageServers);
+		IWorkbenchSite site = HandlerUtil.getActiveSite(event);
+		if (site == null) {
+			return null;
+		}
+		LSPSymbolInWorkspaceDialog dialog = new LSPSymbolInWorkspaceDialog(site.getShell(), languageServers);
 		if (dialog.open() != IDialogConstants.OK_ID) {
 			return null;
 		}
@@ -69,6 +72,18 @@ public class LSPSymbolInWorkspaceHandler extends AbstractHandler {
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 		LSPEclipseUtils.openInEditor(location, page);
 		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		IWorkbenchPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
+		if (part instanceof ITextEditor) {
+			List<LSPDocumentInfo> infos = LanguageServiceAccessor.getLSPDocumentInfosFor(
+					LSPEclipseUtils.getDocument((ITextEditor) part),
+					(capabilities) -> Boolean.TRUE.equals(capabilities.getWorkspaceSymbolProvider()));
+			return !infos.isEmpty();
+		}
+		return false;
 	}
 
 }
