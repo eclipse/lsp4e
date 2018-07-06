7386ea72a2a62f01f3db3e09d10cc1489bc9238b
Lucas Bullen <lbullen@redhat.com>
2018-07-06 15:55:35 -0400

    Bug 528333 - [diagnostic] Performance problem with diagnostics
     - Fix getExisitingMarkerFor comparison
     - use existing but outdated markers to show new diagnostics instead of
    deleting them and creating new ones
     - perform all updates to a marker at once to only redraw once
    
    Change-Id: I99f6f1aeed55aa82df80714430a0afa9a6d521b5
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

--- org.eclipse.lsp4e.test/src/org/eclipse/lsp4e/test/diagnostics/DiagnosticsTest.java
+++ org.eclipse.lsp4e.test/src/org/eclipse/lsp4e/test/diagnostics/DiagnosticsTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Rogue Wave Software Inc. and others.
+ * Copyright (c) 2017, 2018 Rogue Wave Software Inc. and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *  Michał Niewrzał (Rogue Wave Software Inc.) - initial implementation
+ *  Lucas Bullen (Red Hat Inc.) - [Bug 528333] Performance problem with diagnostics
  *******************************************************************************/
 package org.eclipse.lsp4e.test.diagnostics;
 
@@ -16,13 +17,20 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.IntSupplier;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.text.tests.util.DisplayHelper;
 import org.eclipse.lsp4e.operations.diagnostics.LSPDiagnosticsToMarkers;
 import org.eclipse.lsp4e.test.TestUtils;
 import org.eclipse.lsp4e.tests.mock.MockLanguageSever;
@@ -31,6 +39,7 @@ import org.eclipse.lsp4j.DiagnosticSeverity;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.PublishDiagnosticsParams;
 import org.eclipse.lsp4j.Range;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.texteditor.MarkerUtilities;
 import org.junit.After;
 import org.junit.Before;
@@ -44,7 +53,7 @@ public class DiagnosticsTest {
 	@Before
 	public void setUp() throws CoreException {
 		project = TestUtils.createProject("DiagnoticsTest" + System.currentTimeMillis());
-		diagnosticsToMarkers = new LSPDiagnosticsToMarkers(project, "dummy");
+		diagnosticsToMarkers = new LSPDiagnosticsToMarkers("dummy");
 	}
 
 	@After
@@ -78,7 +87,7 @@ public class DiagnosticsTest {
 			assertEquals(0, MarkerUtilities.getCharStart(marker));
 			assertEquals(10, MarkerUtilities.getCharEnd(marker));
 			assertEquals(1, MarkerUtilities.getLineNumber(marker));
-			// TODO compare code, severity, source
+			assertEquals(marker.getAttribute(LSPDiagnosticsToMarkers.LSP_DIAGNOSTIC), diagnostic);
 		}
 
 		diagnosticsToMarkers.accept(new PublishDiagnosticsParams(file.getLocationURI().toString(), Collections.emptyList()));
@@ -110,8 +119,7 @@ public class DiagnosticsTest {
 			assertEquals(content.length(), MarkerUtilities.getCharStart(marker));
 			assertEquals(content.length(), MarkerUtilities.getCharEnd(marker));
 			assertEquals(1, MarkerUtilities.getLineNumber(marker));
-
-			// TODO compare code, severity, source
+			assertEquals(marker.getAttribute(LSPDiagnosticsToMarkers.LSP_DIAGNOSTIC), diagnostic);
 		}
 	}
 
@@ -128,6 +136,83 @@ public class DiagnosticsTest {
 		Thread.sleep(300); //give some time to LSs to process
 		markers = file.findMarkers(LSPDiagnosticsToMarkers.LS_DIAGNOSTIC_MARKER_TYPE, true, IResource.DEPTH_ZERO);
 		assertEquals("there should be 1 marker for each language server", 2, markers.length);
+	}
+
+	@Test
+	public void testDiagnosticRedrawingCalls() throws CoreException {
+		IFile file = TestUtils.createUniqueTestFile(project, "Diagnostic Other Text\nDiagnostic Other Text");
+
+		final Range range1 = new Range(new Position(0, 0), new Position(0, 10));
+		final Range range2 = new Range(new Position(1, 0), new Position(1, 10));
+		final Diagnostic pos1Info1 = createDiagnostic("1", "message1", range1, DiagnosticSeverity.Error, "source1");
+		final Diagnostic pos1Info2 = createDiagnostic("2", "message2", range1, DiagnosticSeverity.Error, "source2");
+		final Diagnostic pos2Info2 = createDiagnostic("2", "message2", range2, DiagnosticSeverity.Error, "source2");
+		// Add first
+		confirmResourceChanges(file, pos1Info1, 1);
+		// Info changed
+		confirmResourceChanges(file, pos1Info2, 1);
+		// Location changed
+		confirmResourceChanges(file, pos2Info2, 1);
+		// Location and Info changed
+		confirmResourceChanges(file, pos1Info1, 1);
+		// Nothing has changed (0 resource changes)
+		confirmResourceChanges(file, pos1Info1, 0);
+	}
+
+	private class MarkerRedrawCountListener implements IResourceChangeListener, IntSupplier {
+		private int resourceChanges = 0;
+
+		@Override
+		public int getAsInt() {
+			return resourceChanges;
+		}
+
+		@Override
+		public void resourceChanged(IResourceChangeEvent event) {
+			// Confirm that it is a marker change
+			IResourceDelta delta = event.getDelta();
+			if (delta == null)
+				return;
+			IResourceDelta[] childDeltas = delta.getAffectedChildren();
+			if (childDeltas.length == 0)
+				return;
+			childDeltas = childDeltas[0].getAffectedChildren();
+			if (childDeltas.length == 0 || childDeltas[0].getMarkerDeltas().length == 0)
+				return;
+			resourceChanges++;
+		}
+
+	}
+
+	private void confirmResourceChanges(IFile file, Diagnostic diagnostic, int expectedChanges) {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		MarkerRedrawCountListener redrawCountListener = new MarkerRedrawCountListener();
+		workspace.addResourceChangeListener(redrawCountListener);
+		try {
+			diagnosticsToMarkers.accept(new PublishDiagnosticsParams(file.getLocationURI().toString(),
+					Collections.singletonList(diagnostic)));
+			new DisplayHelper() {
+				@Override
+				protected boolean condition() {
+					try {
+						IMarker[] markers = file.findMarkers(LSPDiagnosticsToMarkers.LS_DIAGNOSTIC_MARKER_TYPE, false,
+								IResource.DEPTH_INFINITE);
+						for (IMarker marker : markers) {
+							if (marker.getAttribute(LSPDiagnosticsToMarkers.LSP_DIAGNOSTIC).equals(diagnostic)) {
+								return true;
+							}
+						}
+					} catch (CoreException e) {
+						// Caught with False return
+					}
+					return false;
+				}
+			}.waitForCondition(Display.getCurrent(), 5000);
+			assertEquals(expectedChanges, redrawCountListener.getAsInt());
+		} finally {
+			workspace.removeResourceChangeListener(redrawCountListener);
+		}
+
 	}
 
 	private Diagnostic createDiagnostic(String code, String message, Range range, DiagnosticSeverity severity,
--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/diagnostics/LSPDiagnosticsToMarkers.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/diagnostics/LSPDiagnosticsToMarkers.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Red Hat Inc. and others.
+ * Copyright (c) 2016, 2018 Red Hat Inc. and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -8,11 +8,15 @@
  *
  * Contributors:
  *  Mickael Istria (Red Hat Inc.) - initial implementation
+ *  Lucas Bullen (Red Hat Inc.) - [Bug 528333] Performance problem with diagnostics
  *******************************************************************************/
 package org.eclipse.lsp4e.operations.diagnostics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -25,7 +29,10 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jface.text.BadLocationException;
@@ -72,18 +79,34 @@ public class LSPDiagnosticsToMarkers implements Consumer<PublishDiagnosticsParam
 					Arrays.asList(resource.findMarkers(LS_DIAGNOSTIC_MARKER_TYPE, false, IResource.DEPTH_ONE)));
 			remainingMarkers
 					.removeIf(marker -> !Objects.equals(marker.getAttribute(LANGUAGE_SERVER_ID, ""), languageServerId)); //$NON-NLS-1$
+			List<Diagnostic> newDiagnostics = new ArrayList<>();
 			for (Diagnostic diagnostic : diagnostics.getDiagnostics()) {
 				IMarker associatedMarker = getExistingMarkerFor(resource, diagnostic, remainingMarkers);
 				if (associatedMarker == null) {
-					associatedMarker = resource.createMarker(LS_DIAGNOSTIC_MARKER_TYPE);
+					newDiagnostics.add(diagnostic);
 				} else {
 					remainingMarkers.remove(associatedMarker);
 				}
-				updateMarker(resource, diagnostic, associatedMarker);
 			}
-			for (IMarker marker : remainingMarkers) {
-				marker.delete();
-			}
+			IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
+				@Override
+				public void run(IProgressMonitor monitor) throws CoreException {
+					Iterator<IMarker> markerIterator = remainingMarkers.iterator();
+					for (Diagnostic diagnostic : newDiagnostics) {
+						IMarker marker;
+						if (markerIterator.hasNext()) {
+							marker = markerIterator.next();
+						} else {
+							marker = resource.createMarker(LS_DIAGNOSTIC_MARKER_TYPE);
+						}
+						updateMarker(resource, diagnostic, marker);
+					}
+					while (markerIterator.hasNext()) {
+						markerIterator.next().delete();
+					}
+				}
+			};
+			ResourcesPlugin.getWorkspace().run(runnable, new NullProgressMonitor());
 		} catch (CoreException ex) {
 			LanguageServerPlugin.logError(ex);
 		}
@@ -91,10 +114,6 @@ public class LSPDiagnosticsToMarkers implements Consumer<PublishDiagnosticsParam
 
 	protected void updateMarker(IResource resource, Diagnostic diagnostic, IMarker marker) {
 		try {
-			marker.setAttribute(LSP_DIAGNOSTIC, diagnostic);
-			marker.setAttribute(LANGUAGE_SERVER_ID, this.languageServerId);
-			marker.setAttribute(IMarker.MESSAGE, diagnostic.getMessage());
-			marker.setAttribute(IMarker.SEVERITY, LSPEclipseUtils.toEclipseMarkerSeverity(diagnostic.getSeverity()));
 			if (resource.getType() != IResource.FILE) {
 				return;
 			}
@@ -119,10 +138,25 @@ public class LSPDiagnosticsToMarkers implements Consumer<PublishDiagnosticsParam
 					end--;
 				}
 			}
-
-			marker.setAttribute(IMarker.CHAR_START, start);
-			marker.setAttribute(IMarker.CHAR_END, end);
-			marker.setAttribute(IMarker.LINE_NUMBER, document.getLineOfOffset(start) + 1);
+			marker.setAttributes(
+					new String[] {
+							LSP_DIAGNOSTIC,
+							LANGUAGE_SERVER_ID,
+							IMarker.MESSAGE,
+							IMarker.SEVERITY,
+							IMarker.CHAR_START,
+							IMarker.CHAR_END,
+							IMarker.LINE_NUMBER
+					},
+					new Object[] {
+							diagnostic,
+							this.languageServerId,
+							diagnostic.getMessage(),
+							LSPEclipseUtils.toEclipseMarkerSeverity(diagnostic.getSeverity()),
+							start,
+							end,
+							document.getLineOfOffset(start) + 1
+					});
 		} catch (CoreException | BadLocationException e) {
 			LanguageServerPlugin.logError(e);
 		}
@@ -140,9 +174,8 @@ public class LSPDiagnosticsToMarkers implements Consumer<PublishDiagnosticsParam
 			int startOffset = MarkerUtilities.getCharStart(marker);
 			int endOffset = MarkerUtilities.getCharEnd(marker);
 			try {
-				if (marker.getResource().getProjectRelativePath().toString().equals(diagnostic.getSource())
-						&& LSPEclipseUtils.toOffset(diagnostic.getRange().getStart(), document) == startOffset + 1
-						&& LSPEclipseUtils.toOffset(diagnostic.getRange().getEnd(), document) == endOffset + 1
+				if (LSPEclipseUtils.toOffset(diagnostic.getRange().getStart(), document) == startOffset
+						&& LSPEclipseUtils.toOffset(diagnostic.getRange().getEnd(), document) == endOffset
 						&& Objects.equals(marker.getAttribute(IMarker.MESSAGE), diagnostic.getMessage())
 						&& Objects.equals(marker.getAttribute(LANGUAGE_SERVER_ID), this.languageServerId)) {
 					return marker;
