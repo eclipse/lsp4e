e8b761f9fb933c06b2b6add80724ab1a92c95bc9
Jonah Graham <jonah@kichwacoders.com>
2019-09-03 13:31:56 -0400

    Bug 550478: Install toggle breakpoint handler less aggressively
    We had been installing the toggle breakpoint handler (the thing that
    makes clicking on the left hand column install a breakpoint) by registering
    an adapter factory which could get a toggle breakpoint from any editor.
    
    This worked, but was overly aggressive in that this toggle breakpoint
    would become the default handler. That is, unless the editor provided
    its own adapter to IToggleBreakpointsTarget. In the latter case, it would
    not be possible to use LSP4E breakpoints at all.
    
    This change contributes the toggle breakpoint via the org.eclipse.debug.ui.toggleBreakpointsTargetFactories
    extension point instead. That allows all contributions to be collected and
    presented in the "Breakpoint Types" context menu (or via Run) menu and does
    not interfere. The two additional advantages of this are:
    1- When the editor provides IToggleBreakpointsTarget adapter already, we
    can now show the LSP4E version.
    2- Rather than appearing in the UI as "Default" we can display it with our
    own name.
    
    Change-Id: I890ee57f3c10b997441daa459cc894f42ab96a72
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

---
+++ org.eclipse.lsp4e.debug/src/org/eclipse/lsp4e/debug/breakpoints/ToggleBreakpointsTargetFactory.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Kichwa Coders Canada Inc. and others.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.lsp4e.debug.breakpoints;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.debug.ui.actions.IToggleBreakpointsTarget;
+import org.eclipse.debug.ui.actions.IToggleBreakpointsTargetFactory;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.lsp4e.debug.DSPPlugin;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class ToggleBreakpointsTargetFactory implements IToggleBreakpointsTargetFactory {
+
+	private static final DSPBreakpointAdapter DSP_BREAKPOINT_ADAPTER = new DSPBreakpointAdapter();
+
+	public static String TOGGLE_BREAKPOINT_TARGET_ID = DSPPlugin.PLUGIN_ID + ".toggleBreakpointTarget"; //$NON-NLS-1$
+
+	private static Set<String> TOGGLE_TARGET_IDS = Collections.singleton(TOGGLE_BREAKPOINT_TARGET_ID);
+
+	@Override
+	public Set<String> getToggleTargets(IWorkbenchPart part, ISelection selection) {
+		return TOGGLE_TARGET_IDS;
+	}
+
+	@Override
+	public String getDefaultToggleTarget(IWorkbenchPart part, ISelection selection) {
+		return null;
+	}
+
+	@Override
+	public IToggleBreakpointsTarget createToggleTarget(String targetID) {
+		if (TOGGLE_BREAKPOINT_TARGET_ID.equals(targetID)) {
+			return DSP_BREAKPOINT_ADAPTER;
+		}
+		return null;
+	}
+
+	@Override
+	public String getToggleTargetName(String targetID) {
+		if (TOGGLE_BREAKPOINT_TARGET_ID.equals(targetID)) {
+			return "LSP4E Breakpoint";
+		}
+		return null;
+	}
+
+	@Override
+	public String getToggleTargetDescription(String targetID) {
+		if (TOGGLE_BREAKPOINT_TARGET_ID.equals(targetID)) {
+			return "LSP4E Breakpoint for Debug Adapter";
+		}
+		return null;
+
+	}
+
+}
