93b8ad7ce8b267367d09ebb2ef44745209342136
rubenporras <43636626+rubenporras@users.noreply.github.com>
2021-12-07 08:56:45 +0100

    Do not raise exception if the file does not exists
    Instead of that, use only the file name to extract the content types.

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/LSPEclipseUtils.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/LSPEclipseUtils.java
@@ -816,14 +816,19 @@ public class LSPEclipseUtils {
 
 	@NonNull
 	public static List<IContentType> getFileContentTypes(@NonNull IFile file) {
+		IContentTypeManager contentTypeManager = Platform.getContentTypeManager();
 		List<IContentType> contentTypes = new ArrayList<>();
-		try (InputStream contents = file.getContents()) {
-			// TODO consider using document as inputstream
-			contentTypes.addAll(
-					Arrays.asList(Platform.getContentTypeManager().findContentTypesFor(contents, file.getName())));
-		} catch (CoreException | IOException e) {
-			LanguageServerPlugin.logError(e);
-		}
+		if (file.exists()) {
+			try (InputStream contents = file.getContents()) {
+				// TODO consider using document as inputstream
+				contentTypes.addAll(
+						Arrays.asList(contentTypeManager.findContentTypesFor(contents, file.getName())));
+			} catch (CoreException | IOException e) {
+				LanguageServerPlugin.logError(e);
+			}
+		} else {
+			contentTypes.addAll(Arrays.asList(contentTypeManager.findContentTypesFor(file.getName())));
+		}	
 		return contentTypes;
 	}
 
