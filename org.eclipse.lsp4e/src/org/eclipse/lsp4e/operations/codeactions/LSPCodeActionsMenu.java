fae1ac2fe3c4f49f25c08973018248e87436edd3
Ahmed Neil Hussain <ahmed@cocotec.io>
2022-12-12 10:40:15 +0000

    Port LSPCodeActionsMenu to try out LSPExecutor.computeAll()

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/codeactions/LSPCodeActionsMenu.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/codeactions/LSPCodeActionsMenu.java
@@ -12,23 +12,24 @@
  *******************************************************************************/
 package org.eclipse.lsp4e.operations.codeactions;
 
+import java.net.URI;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jface.action.ContributionItem;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.lsp4e.LSPEclipseUtils;
+import org.eclipse.lsp4e.LSPExecutor;
+import org.eclipse.lsp4e.LSPExecutor.LSPDocumentExecutor;
 import org.eclipse.lsp4e.LanguageServerPlugin;
-import org.eclipse.lsp4e.LanguageServiceAccessor;
-import org.eclipse.lsp4e.LanguageServiceAccessor.LSPDocumentInfo;
+import org.eclipse.lsp4e.LanguageServerWrapper;
 import org.eclipse.lsp4e.ui.LSPImages;
 import org.eclipse.lsp4e.ui.Messages;
 import org.eclipse.lsp4e.ui.UI;
@@ -56,8 +57,9 @@ import org.eclipse.ui.texteditor.ITextEditor;
 
 public class LSPCodeActionsMenu extends ContributionItem implements IWorkbenchContribution {
 
-	private List<LSPDocumentInfo> infos;
 	private Range range;
+
+	private LSPDocumentExecutor executor;
 
 	@Override
 	public void initialize(IServiceLocator serviceLocator) {
@@ -68,8 +70,9 @@ public class LSPCodeActionsMenu extends ContributionItem implements IWorkbenchCo
 			if (document == null) {
 				return;
 			}
-			infos = LanguageServiceAccessor.getLSPDocumentInfosFor(document,
-					LSPCodeActionMarkerResolution::providesCodeActions);
+
+			executor = LSPExecutor.forDocument(document);
+			executor.withFilter(LSPCodeActionMarkerResolution::providesCodeActions);
 			ITextSelection selection = (ITextSelection) textEditor.getSelectionProvider().getSelection();
 			try {
 				this.range = new Range(LSPEclipseUtils.toPosition(selection.getOffset(), document),
@@ -84,87 +87,83 @@ public class LSPCodeActionsMenu extends ContributionItem implements IWorkbenchCo
 	public void fill(final Menu menu, int index) {
 		final MenuItem item = new MenuItem(menu, SWT.NONE, index);
 		item.setEnabled(false);
-		if (infos.isEmpty()) {
+
+		final URI fileUri = LSPEclipseUtils.toUri(this.executor.getDocument());
+
+		CodeActionContext context = new CodeActionContext(Collections.emptyList());
+		CodeActionParams params = new CodeActionParams();
+		params.setTextDocument(new TextDocumentIdentifier(fileUri.toString()));
+		params.setRange(this.range);
+		params.setContext(context);
+
+		final @NonNull List<@NonNull CompletableFuture<List<Either<Command, CodeAction>>>> actions
+			= executor.computeAll((w, ls) -> ls.getTextDocumentService().codeAction(params)
+					.whenComplete((codeActions, t) -> scheduleMenuUpdate(menu, index, w, t, codeActions)));
+
+		if (actions.isEmpty()) {
 			item.setText(Messages.notImplemented);
 			return;
 		}
-
 		item.setText(Messages.computing);
-		CodeActionContext context = new CodeActionContext(Collections.emptyList());
-		CodeActionParams params = new CodeActionParams();
-		params.setTextDocument(new TextDocumentIdentifier(infos.get(0).getFileUri().toString()));
-		params.setRange(this.range);
-		params.setContext(context);
-		Set<CompletableFuture<?>> runningFutures = new HashSet<>();
-		for (LSPDocumentInfo info : this.infos) {
-			final CompletableFuture<List<Either<Command, CodeAction>>> codeActions = info.getInitializedLanguageClient()
-					.thenComposeAsync(languageServer -> languageServer.getTextDocumentService().codeAction(params));
-			runningFutures.add(codeActions);
-			codeActions.whenComplete((t, u) -> {
-				runningFutures.remove(codeActions);
-				UIJob job = new UIJob(menu.getDisplay(), Messages.updateCodeActions_menu) {
-					@Override
-					public IStatus runInUIThread(IProgressMonitor monitor) {
-						if (u != null) {
-							final MenuItem item = new MenuItem(menu, SWT.NONE, index);
-							item.setText(u.getMessage());
-							item.setImage(LSPImages.getSharedImage(ISharedImages.IMG_DEC_FIELD_ERROR));
-							item.setEnabled(false);
-						} else if (t != null) {
-							for (Either<Command, CodeAction> command : t) {
-								if (command != null) {
-									if (command.isLeft()) {
-										final MenuItem item = new MenuItem(menu, SWT.NONE, index);
-										item.setText(command.getLeft().getTitle());
-										item.addSelectionListener(new SelectionAdapter() {
-											@Override
-											public void widgetSelected(SelectionEvent e) {
-												executeCommand(info, command.getLeft());
-											}
-										});
-									} else if (command.isRight()) {
-										CodeAction codeAction = command.getRight();
-										final MenuItem item = new MenuItem(menu, SWT.NONE, index);
-										item.setText(codeAction.getTitle());
-										item.addSelectionListener(new SelectionAdapter() {
-											@Override
-											public void widgetSelected(SelectionEvent e) {
-												if (codeAction.getEdit() != null) {
-													LSPEclipseUtils.applyWorkspaceEdit(codeAction.getEdit(), codeAction.getTitle());
-												}
-												if (codeAction.getCommand() != null) {
-													executeCommand(info, codeAction.getCommand());
-												}
-											}
-										});
-									}
-								}
-							}
-						}
-						if (menu.getItemCount() == 1) {
-							item.setText(Messages.codeActions_emptyMenu);
-						} else {
-							item.dispose();
-						}
-						return Status.OK_STATUS;
-					}
-				};
-				job.schedule();
-			});
-		}
+
 		super.fill(menu, index);
 	}
 
-	private void executeCommand(LSPDocumentInfo info, Command command) {
-		ServerCapabilities capabilities = info.getCapabilites();
+	private void scheduleMenuUpdate(final Menu menu, final int index, final LanguageServerWrapper wrapper, final Throwable u, final List<Either<Command, CodeAction>> codeActions) {
+		UIJob job = new UIJob(menu.getDisplay(), Messages.updateCodeActions_menu) {
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				if (u != null) {
+					final MenuItem item = new MenuItem(menu, SWT.NONE, index);
+					item.setText(u.getMessage());
+					item.setImage(LSPImages.getSharedImage(ISharedImages.IMG_DEC_FIELD_ERROR));
+					item.setEnabled(false);
+				} else if (codeActions != null) {
+					for (Either<Command, CodeAction> command : codeActions) {
+						if (command != null) {
+							if (command.isLeft()) {
+								final MenuItem item = new MenuItem(menu, SWT.NONE, index);
+								item.setText(command.getLeft().getTitle());
+								item.addSelectionListener(new SelectionAdapter() {
+									@Override
+									public void widgetSelected(SelectionEvent e) {
+										executeCommand(wrapper, command.getLeft());
+									}
+								});
+							} else if (command.isRight()) {
+								CodeAction codeAction = command.getRight();
+								final MenuItem item = new MenuItem(menu, SWT.NONE, index);
+								item.setText(codeAction.getTitle());
+								item.addSelectionListener(new SelectionAdapter() {
+									@Override
+									public void widgetSelected(SelectionEvent e) {
+										if (codeAction.getEdit() != null) {
+											LSPEclipseUtils.applyWorkspaceEdit(codeAction.getEdit(), codeAction.getTitle());
+										}
+										if (codeAction.getCommand() != null) {
+											executeCommand(wrapper, codeAction.getCommand());
+										}
+									}
+								});
+							}
+						}
+					}
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+	}
+
+	private void executeCommand(final LanguageServerWrapper wrapper, final Command command) {
+		final ServerCapabilities capabilities = wrapper.getServerCapabilities();
 		if (capabilities != null) {
 			ExecuteCommandOptions provider = capabilities.getExecuteCommandProvider();
 			if (provider != null && provider.getCommands().contains(command.getCommand())) {
 				ExecuteCommandParams params = new ExecuteCommandParams();
 				params.setCommand(command.getCommand());
 				params.setArguments(command.getArguments());
-				info.getInitializedLanguageClient()
-						.thenAcceptAsync(ls -> ls.getWorkspaceService().executeCommand(params));
+				wrapper.execute(ls -> ls.getWorkspaceService().executeCommand(params));
 			}
 		}
 	}
