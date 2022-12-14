eclipse/lsp4e

commit dab2eb290eb860f6086e7cb090cdcfdfeb7dfb7d

Currently the log is using currentTimeMillis, see below
[t=1667223932171] LSP4E to ...:

For debugging and readability this is not useful, as usually one would
like to correlate logs from the client and the server, instead use the
standard:

2022-11-29T13:20:11.220291200+01:00 LSP4E to ...:

org.eclipse.lsp4e/src/org/eclipse/lsp4e/LoggingStreamConnectionProviderProxy.java
@@ -20,6 +20,7 @@ import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
+import java.time.OffsetDateTime;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
@@ -108,6 +109,29 @@ public class LoggingStreamConnectionProviderProxy implements StreamConnectionPro
 		this.logFile = getLogFile();
 	}
 
+
+	private String message(String direction, byte[] payload) {
+		String now = OffsetDateTime.now().toString();
+		StringBuilder builder = new StringBuilder(payload.length + id.length() + direction.length() + now.length() + 10);
+		builder.append("\n["); //$NON-NLS-1$
+		builder.append(now);
+		builder.append("] "); //$NON-NLS-1$
+		builder.append(direction);
+		builder.append(' ');
+		builder.append(id);
+		builder.append(":\n"); //$NON-NLS-1$
+		builder.append(payload);
+		return builder.toString();
+	}
+
+	private String infoMessage(byte[] payload) {
+		return message("LSP4E to", payload);  //$NON-NLS-1$
+	}
+
+	private String errorMessage(byte[] payload) {
+		return message("Error from", payload);  //$NON-NLS-1$
+	}
+
 	@Override
 	public InputStream getInputStream() {
 		if (inputStream != null) {
@@ -121,7 +145,7 @@ public class LoggingStreamConnectionProviderProxy implements StreamConnectionPro
 					byte[] payload = new byte[bytes];
 					System.arraycopy(b, off, payload, 0, bytes);
 					if (logToConsole || logToFile) {
-						String s = "\n[t=" + System.currentTimeMillis() + "] " + id + " to LSP4E:\n" + new String(payload); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+						String s = infoMessage(payload);
 						if (logToConsole) {
 							logToConsole(s);
 						}
@@ -149,7 +173,7 @@ public class LoggingStreamConnectionProviderProxy implements StreamConnectionPro
 					byte[] payload = new byte[bytes];
 					System.arraycopy(b, off, payload, 0, bytes);
 					if (logToConsole || logToFile) {
-						String s = "\n[t=" + System.currentTimeMillis() + "] Error from " + id + ":\n" + new String(payload); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+						String s = errorMessage(payload);
 						if (logToConsole) {
 							logToConsole(s);
 						}
@@ -174,7 +198,7 @@ public class LoggingStreamConnectionProviderProxy implements StreamConnectionPro
 				@Override
 				public void write(byte[] b) throws IOException {
 					if (logToConsole || logToFile) {
-						String s = "\n[t=" + System.currentTimeMillis() + "] LSP4E to " + id + ":\n" + new String(b);  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+						String s = infoMessage(b);
 						if (logToConsole) {
 							logToConsole(s);
 						}

commit f91c32d946af12f31f6c3d6e15e67bca76f43ba2

Fix regression introduced in dab2eb290eb860f6086e7cb090cdcfdfeb7dfb7d

org.eclipse.lsp4e/src/org/eclipse/lsp4e/LoggingStreamConnectionProviderProxy.java
@@ -120,7 +120,7 @@ public class LoggingStreamConnectionProviderProxy implements StreamConnectionPro
 		builder.append(' ');
 		builder.append(id);
 		builder.append(":\n"); //$NON-NLS-1$
-		builder.append(payload);
+		builder.append(new String(payload));
 		return builder.toString();
 	}
 