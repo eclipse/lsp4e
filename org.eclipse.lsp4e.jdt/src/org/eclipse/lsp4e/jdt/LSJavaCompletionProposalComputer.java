d37a83bb2c9e41bd8ed82eebbab31c97fe45dcc8
Sebastian Thomschke <sebthom@users.noreply.github.com>
2023-01-30 14:11:42 +0100

    add missing final keywords to static fields

--- org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaCompletionProposalComputer.java
+++ org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaCompletionProposalComputer.java
@@ -29,9 +29,9 @@ import org.eclipse.lsp4e.operations.completion.LSContentAssistProcessor;
 
 @SuppressWarnings({ "restriction" })
 public class LSJavaCompletionProposalComputer implements IJavaCompletionProposalComputer {
-	
-	private static TimeUnit TIMEOUT_UNIT = TimeUnit.MILLISECONDS;
-	private static long TIMEOUT_LENGTH = 300;
+
+	private static final TimeUnit TIMEOUT_UNIT = TimeUnit.MILLISECONDS;
+	private static final long TIMEOUT_LENGTH = 300;
 
 	private LSContentAssistProcessor lsContentAssistProcessor;
 	private String javaCompletionSpecificErrorMessage;
@@ -50,7 +50,7 @@ public class LSJavaCompletionProposalComputer implements IJavaCompletionProposal
 		CompletableFuture<ICompletionProposal[]> future = CompletableFuture.supplyAsync(() -> {
 			return lsContentAssistProcessor.computeCompletionProposals(context.getViewer(), context.getInvocationOffset());
 		});
-		
+
 		try {
 			return Arrays.asList(asJavaProposals(future));
 		} catch (TimeoutException e) {
@@ -78,24 +78,24 @@ public class LSJavaCompletionProposalComputer implements IJavaCompletionProposal
 	 * The LSPCompletionProposal that LSP4E computes is NOT IJavaCompletionProposal, and as a consequence JDT
 	 * will by default sort any non Java proposals by display value, which is why we would get a strange sorting order,
 	 * even if our the LS and LSP4E both return a proposal list in the right order.
-	 * 
+	 *
 	 * This method wraps around the LSCompletionProposal with a IJavaCompletionProposal, and it sets the relevance
 	 * number that JDT uses to sort proposals in a desired order.
 	 */
 	private ICompletionProposal[] asJavaProposals(CompletableFuture<ICompletionProposal[]> future)
 			throws InterruptedException, ExecutionException, TimeoutException {
 		ICompletionProposal[] originalProposals = future.get(TIMEOUT_LENGTH, TIMEOUT_UNIT);
-		
+
 		// We assume that the original proposals are in the correct order, so we set relevance
 		// based on this existing order. Note that based on IJavaCompletionProposal javadoc,
 		// relevance values are [0,1000] so we start at 1000
 		int relevance = 1000;
 		ICompletionProposal[] javaProposals = new ICompletionProposal[originalProposals.length];
-		
+
 		for (int i = 0; i < originalProposals.length; i++) {
 			javaProposals[i] = new LSJavaProposal(originalProposals[i], relevance--);
 		}
-		 
+
 		return javaProposals;
 	}
 
@@ -105,7 +105,7 @@ public class LSJavaCompletionProposalComputer implements IJavaCompletionProposal
 		IContextInformation[] contextInformation = lsContentAssistProcessor.computeContextInformation(context.getViewer(), context.getInvocationOffset());
 		return Arrays.asList(contextInformation);
 	}
-	
+
 	@Override
 	public String getErrorMessage() {
 		return javaCompletionSpecificErrorMessage != null ? javaCompletionSpecificErrorMessage : lsContentAssistProcessor.getErrorMessage();
