1e3e5afe069bedcbd880fafaba885343aa8cc044
Ahmed Neil Hussain <ahmed@cocotec.io>
2023-08-09 18:58:20 +0100

    Use custom context to scope key bindings for language server

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/LanguageServerPlugin.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/LanguageServerPlugin.java
@@ -11,17 +11,31 @@
  *******************************************************************************/
 package org.eclipse.lsp4e;
 
+
+import java.util.WeakHashMap;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.lsp4e.ui.LSPImages;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPartListener2;
+import org.eclipse.ui.IPartService;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartReference;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.contexts.IContextActivation;
+import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 public class LanguageServerPlugin extends AbstractUIPlugin {
 
 	public static final String PLUGIN_ID = "org.eclipse.lsp4e"; //$NON-NLS-1$
+
+	public static final String CONTEXT_ID = "org.eclipse.lsp4e.context"; //$NON-NLS-1$
 
 	public static final boolean DEBUG = Boolean.parseBoolean(Platform.getDebugOption("org.eclipse.lsp4e/debug")); //$NON-NLS-1$
 
@@ -31,10 +45,53 @@ public class LanguageServerPlugin extends AbstractUIPlugin {
 	public LanguageServerPlugin() {
 	}
 
+	public static class WindowListener implements IPartListener2 {
+		private final IContextService service;
+
+		private final WeakHashMap<IWorkbenchPart, IContextActivation> activations = new WeakHashMap<>();
+		public WindowListener(IContextService service) {
+			this.service = service;
+			}
+
+		@Override public void
+		partActivated(IWorkbenchPartReference partRef) {
+			IWorkbenchPart part = partRef.getPart(false);
+			if (part instanceof IEditorPart) {
+				IEditorPart editorPart = (IEditorPart)part;
+				if (LanguageServersRegistry.getInstance().canUseLanguageServer(editorPart.getEditorInput())) {
+					IContextActivation activation = this.service.activateContext(CONTEXT_ID);
+					this.activations.put(editorPart, activation);
+				}
+			}
+
+		}
+
+		@Override public void
+		partDeactivated(IWorkbenchPartReference partRef) {
+			IWorkbenchPart part = partRef.getPart(false);
+
+			if (part instanceof IEditorPart) {
+				IEditorPart editorPart = (IEditorPart)part;
+				IContextActivation activation = this.activations.remove(editorPart);
+				if (activation != null) {
+					this.service.deactivateContext(activation);
+				}
+			}
+		}
+	}
+
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IPartService partService = window.getPartService();
+		IContextService contextService = PlatformUI.getWorkbench()
+				.getService(IContextService.class);
+		IPartListener2 listener = new WindowListener(contextService);
+		partService.addPartListener(listener);
+
+
 	}
 
 	@Override
