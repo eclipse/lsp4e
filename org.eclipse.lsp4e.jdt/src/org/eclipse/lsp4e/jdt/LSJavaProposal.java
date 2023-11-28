9d3d3e85321484e3a14b64b2b8da354c5f07de17
Martin Lippert <mlippert@gmail.com>
2023-11-28 13:37:11 +0100

    let LSJavaCompletionProposal implement extension interface to enable linked editing mode to work
    Change-Id: I11755f126f9d851b316680a4ca4342fcd6bce36f

--- org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaProposal.java
+++ org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaProposal.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 VMware Inc. and others.
+ * Copyright (c) 2022, 2023 VMware Inc. and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -12,13 +12,18 @@
 package org.eclipse.lsp4e.jdt;
 
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
+import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposalExtension2;
 import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.lsp4e.operations.completion.LSCompletionProposal;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 
-class LSJavaProposal implements IJavaCompletionProposal {
+@SuppressWarnings("restriction")
+class LSJavaProposal implements IJavaCompletionProposal, ICompletionProposalExtension2 {
 	
 	private ICompletionProposal delegate;
 	private int relevance;
@@ -61,6 +66,35 @@ class LSJavaProposal implements IJavaCompletionProposal {
 	@Override
 	public int getRelevance() {
 		return relevance;
+	}
+
+	@Override
+	public void apply(ITextViewer viewer, char trigger, int stateMask, int offset) {
+		if (delegate instanceof LSCompletionProposal) {
+			((LSCompletionProposal) delegate).apply(viewer, trigger, stateMask, offset);
+		}
+	}
+
+	@Override
+	public void selected(ITextViewer viewer, boolean smartToggle) {
+		if (delegate instanceof LSCompletionProposal) {
+			((LSCompletionProposal) delegate).selected(viewer, smartToggle);
+		}
+	}
+
+	@Override
+	public void unselected(ITextViewer viewer) {
+		if (delegate instanceof LSCompletionProposal) {
+			((LSCompletionProposal) delegate).unselected(viewer);
+		}
+	}
+
+	@Override
+	public boolean validate(IDocument document, int offset, DocumentEvent event) {
+		if (delegate instanceof LSCompletionProposal) {
+			return ((LSCompletionProposal) delegate).validate(document, offset, event);
+		}
+		return false;
 	}
 	
 }
