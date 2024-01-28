c60603e1de49159bd208897ca38bafe7bf03dc82
Gesa Hentschke <gesa.hentschke@bachmann.info>
2024-01-28 13:04:06 +0100

    [#902] Do not split line endings in edits
    Add check in case that end line from edit is larger than the documents
    number of lines.
    
    fixes #902

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/LSPEclipseUtils.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/LSPEclipseUtils.java
@@ -524,8 +524,11 @@ public final class LSPEclipseUtils {
 				// check if that edit would actually change the document
 				if (!document.get(offset, length).equals(textEdit.getNewText())) {
 					var newText = textEdit.getNewText();
+					var zeroBasedDocumentLines = Math.max(0, document.getNumberOfLines() - 1);
+					var endLine = textEdit.getRange().getEnd().getLine();
+					endLine = endLine > zeroBasedDocumentLines ? zeroBasedDocumentLines : endLine;
 					// Do not split "\r\n" line ending:
-					if ("\r\n".equals(document.getLineDelimiter(textEdit.getRange().getEnd().getLine()))) { //$NON-NLS-1$;
+					if ("\r\n".equals(document.getLineDelimiter(endLine))) { //$NON-NLS-1$;
 						// if last char in the newText is a carriage return:
 						if ('\r' == newText.charAt(newText.length()-1) && offset + length < document.getLength()) {
 							// replace the whole line:
