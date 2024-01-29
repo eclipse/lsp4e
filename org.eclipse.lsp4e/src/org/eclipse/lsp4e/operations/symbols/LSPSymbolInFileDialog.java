8a3ff8ffe06928999dd493b48a201b4cddc7601f
Dietrich Travkin <dietrich.travkin@solunar.de>
2024-01-29 16:15:41 +0100

    Close quick outline after selection, but before updating editor's cursor position

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/symbols/LSPSymbolInFileDialog.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/symbols/LSPSymbolInFileDialog.java
@@ -148,6 +148,8 @@ public class LSPSymbolInFileDialog extends PopupDialog {
 		Object selectedElement= getSelectedElement();
 
 		if (selectedElement != null) {
+			close();
+
 			Range range = null;
 			if (selectedElement instanceof SymbolInformation symbolInformation) {
 				range = symbolInformation.getLocation().getRange();
@@ -165,7 +167,6 @@ public class LSPSymbolInFileDialog extends PopupDialog {
 				} catch (BadLocationException e) {
 					LanguageServerPlugin.logError(e);
 				}
-				close();
 			}
 		}
 	}
