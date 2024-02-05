8853dcf326a819141bc9e1a14aa9c6526940e89d
aboyko <aboyko@vmware.com>
2024-02-05 18:02:01 -0500

    JDT completions coming from LS application fixed

--- org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LspJavaQuickAssistProcessor.java
+++ org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LspJavaQuickAssistProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 VMware Inc. and others.
+ * Copyright (c) 2022, 2024 VMware Inc. and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -17,11 +17,14 @@ import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.jdt.ui.text.java.IProblemLocation;
 import org.eclipse.jdt.ui.text.java.IQuickAssistProcessor;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposalExtension;
+import org.eclipse.jface.text.contentassist.ICompletionProposalExtension2;
 import org.eclipse.jface.text.quickassist.IQuickAssistInvocationContext;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.TextInvocationContext;
 import org.eclipse.lsp4e.operations.codeactions.LSPCodeActionQuickAssistProcessor;
 
+@SuppressWarnings("restriction")
 public class LspJavaQuickAssistProcessor extends LSPCodeActionQuickAssistProcessor implements IQuickAssistProcessor {
 
 	private static final int RELEVANCE = 100;
@@ -59,7 +62,17 @@ public class LspJavaQuickAssistProcessor extends LSPCodeActionQuickAssistProcess
 		ICompletionProposal[] proposals = computeQuickAssistProposals(getContext(context));
 		final var javaProposals = new IJavaCompletionProposal[proposals.length];
 		for (int i = 0; i < proposals.length; i++) {
-			javaProposals[i] = new LSJavaProposal(proposals[i], RELEVANCE);
+			/*
+			 * Different completion extension applied differently, hence each requires a wrapper implementing 
+			 * proper completion proposal extension
+			 */
+			if (proposals[i] instanceof ICompletionProposalExtension2) {
+				javaProposals[i] = new LSJavaProposalExtension2(proposals[i], RELEVANCE);
+			} else if (proposals[i] instanceof ICompletionProposalExtension) {
+				javaProposals[i] = new LSJavaProposalExtension(proposals[i], RELEVANCE);
+			} else {
+				javaProposals[i] = new LSJavaProposal(proposals[i], RELEVANCE);
+			}
 		}
 		return javaProposals;
 	}
